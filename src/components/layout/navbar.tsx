107049f67acde6039a522a23885f5a527b3ecac6
Divine Monday <apostlediv@gmail.com>
2026-06-08 09:49:37 +0100

    fix: prevent background scrolling when mobile drawer is open

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -71,11 +71,14 @@ export function Navbar() {
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
     }
     return () => {
       document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
     };
   }, [isOpen]);
 
